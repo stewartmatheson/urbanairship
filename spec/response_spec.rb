f8c5aacd9dc25b2044de27cc8e00b60038b379e5
Stewart Matheson <stewart.matheson@gmail.com>
2012-03-13 12:07:41 +1100

    fixed response spec to use client

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -1,56 +1,83 @@
 describe Urbanairship::Response do
   before do
+    FakeWeb.clean_registry
     FakeWeb.allow_net_connect = false
   end
 
   context "#code" do
-    subject { Urbanairship.register_device("new_device_token") }
+    
+    subject {
+      Urbanairship.configure do |config|
+        config.application_key = "my_app_key"
+        config.application_secret = "my_app_secret"
+        config.platform = :ios
+      end
+    }
 
     before do
       FakeWeb.register_uri(:put, "https://my_app_key:my_app_secret@go.urbanairship.com/api/device_tokens/new_device_token", :status => ["201", "Created"])
       FakeWeb.register_uri(:put, /bad_key\:my_app_secret\@go\.urbanairship\.com/, :status => ["401", "Unauthorized"])
-      Urbanairship.application_secret = "my_app_secret"
+      @response = subject.register_device("new_device_token")
     end
 
     it "should be set correctly on new registraion token" do
-      Urbanairship.application_key = "my_app_key"
-      subject.code.should eql '201'
+      @response.code.should eql '201'
     end
 
     it "should set correctly on unauthhorized" do
-      Urbanairship.application_key = "bad_key"
-      subject.code.should eql '401'
+      client = Urbanairship.configure do |config|
+        config.application_key = "bad_key"
+        config.application_secret = "my_app_secret"
+        config.platform = :ios
+      end
+
+      client.register_device("new_device_token").code.should eql '401'
     end
  end
 
   context "#success?" do
-    subject { Urbanairship.register_device("new_device_token") }
+    subject {
+      Urbanairship.configure do |config|
+        config.application_key = "my_app_key"
+        config.application_secret = "my_app_secret"
+        config.platform = :ios
+      end
+    }
 
     before do
       FakeWeb.register_uri(:put, "https://my_app_key:my_app_secret@go.urbanairship.com/api/device_tokens/new_device_token", :status => ["201", "Created"])
       FakeWeb.register_uri(:put, /bad_key\:my_app_secret\@go\.urbanairship\.com/, :status => ["401", "Unauthorized"])
-      Urbanairship.application_secret = "my_app_secret"
+      @response = subject.register_device("new_device_token")
     end
 
     it "should be true with good key" do
-      Urbanairship.application_key = "my_app_key"
-      subject.success?.should == true
+      @response.success?.should == true
     end
 
     it "should be false with bad key" do
-      Urbanairship.application_key = "bad_key"
-      subject.success?.should == false
+      client = Urbanairship.configure do |config|
+        config.application_key = "bad_key"
+        config.application_secret = "my_app_secret"
+        config.platform = :android
+      end
+      client.register_device("new_device_token").success?.should == false
     end
   end
 
   context "body array accessor" do
-    subject { Urbanairship.feedback(Time.now) }
+    let(:client) {
+      Urbanairship.configure  do |config|
+        config.application_secret = "my_app_secret"
+        config.application_key = "my_app_key"
+        config.master_secret = "my_master_secret"
+        config.platform = :android
+      end
+    }
+
+    subject { client.feedback(Time.now) }
 
     before do
       FakeWeb.register_uri(:get, /my_app_key\:my_master_secret\@go\.urbanairship.com\/api\/device_tokens\/feedback/, :status => ["200", "OK"], :body => "[{\"device_token\":\"token\",\"marked_inactive_on\":\"2010-10-14T19:15:13Z\",\"alias\":\"my_alias\"},{\"device_token\":\"token2\",\"marked_inactive_on\":\"2010-10-14T19:15:13Z\",\"alias\":\"my_alias\"}]")
-      Urbanairship.application_secret = "my_app_secret"
-      Urbanairship.application_key = "my_app_key"
-      Urbanairship.master_secret = "my_master_secret"
     end
 
     it "should set up correct indexes" do
@@ -60,13 +87,20 @@ describe Urbanairship::Response do
   end
 
   context "non array response" do
-    subject { Urbanairship.feedback(Time.now) }
+
+    let(:client) {
+      Urbanairship.configure  do |config|
+        config.application_secret = "my_app_secret"
+        config.application_key = "my_app_key"
+        config.master_secret = "my_master_secret"
+        config.platform = :android
+      end
+    }
+
+    subject { client.feedback(Time.now) }
 
     before do
       FakeWeb.register_uri(:get, /my_app_key\:my_master_secret\@go\.urbanairship.com\/api\/device_tokens\/feedback/, :status => ["200", "OK"], :body => "{\"device_token\":\"token\",\"marked_inactive_on\":\"2010-10-14T19:15:13Z\",\"alias\":\"my_alias\"}")
-      Urbanairship.application_secret = "my_app_secret"
-      Urbanairship.application_key = "my_app_key"
-      Urbanairship.master_secret = "my_master_secret"
     end
 
     it "should set up correct keys" do
