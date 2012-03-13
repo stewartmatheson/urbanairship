6a18ad31bc4e36f55246de09dbfda8499fabb721
Stewart Matheson <stewart.matheson@gmail.com>
2012-03-13 12:06:39 +1100

    moved common request code in to client

--- lib/urbanairship/client.rb
+++ lib/urbanairship/client.rb
@@ -6,6 +6,30 @@ module Urbanairship
   
     attr_accessor :request_timeout
 
+    def delete_scheduled_push(param)
+      path = param.is_a?(Hash) ? "/api/push/scheduled/alias/#{param[:alias].to_s}" : "/api/push/scheduled/#{param.to_s}"
+      do_request(:delete, path, :authenticate_with => :master_secret)
+    end
+
+    def push(options = {})
+      body = parse_push_options(options).to_json
+      do_request(:post, "/api/push/", :body => body, :authenticate_with => :master_secret)
+    end
+
+    def batch_push(notifications = [])
+      body = notifications.map{|notification| parse_push_options(notification)}.to_json
+      do_request(:post, "/api/push/batch/", :body => body, :authenticate_with => :master_secret)
+    end
+
+    def broadcast_push(options = {})
+      body = parse_push_options(options).to_json
+      do_request(:post, "/api/push/broadcast/", :body => body, :authenticate_with => :master_secret)
+    end
+
+    def feedback(time)
+      do_request(:get, "/api/device_tokens/feedback/?since=#{format_time(time)}", :authenticate_with => :master_secret)
+    end
+
     private
 
     def parse_register_options(hash = {})
